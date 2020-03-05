d7abc3013ee3b274bb6701ddbf887fa724408b8f
smiletoraphi <friedliraph@gmail.com>
2020-03-05 11:59:27 +0100

    change year input

--- Program.fs
+++ Program.fs
@@ -7,15 +7,12 @@ module testModule=
             let year = DateTime.Now.Year
             year - yearOfBirth
             
-        let estimate1 =  calculateAge(year1)
-        let estimate2 =  calculateAge(year2)
-        let estimate3 =  calculateAge(year3)
-        let averageAge = (estimate1 + estimate2 + estimate3) / 3
+        let averageAge = (calculateAge(year1) + calculateAge(year2) + calculateAge(year3)) / 3
         sprintf "Average age of family %s is %d years" familyName averageAge
 
 [<EntryPoint>]
 let main argv =
 
-    printfn "OUTPUT: %s"  (testModule.estimateAges("Friedli", 1959, 1986, 1995))
+    printfn "OUTPUT: %s"  (testModule.estimateAges("Friedli", 1959, 1986, 1994))
 
     0
